aad877c3d1aaabf1f6e1afa418d1870c73b618df
François <fran.hanesse6@outlook.fr>
2019-10-25 09:54:05 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Contact from './components/Contact';
+import './App.css';
+
+const name = "Dwayne Ferguson";
+const avatar = "https://randomuser.me/api/portraits/men/83.jpg"
+const status = false;
+
+
+const name1 = "Melinda Harper";
+const avatar1= "https://randomuser.me/api/portraits/women/8.jpg";
+const status1 = true;
+
+const name2 = "Clyde Carroll";
+const avatar2= "https://randomuser.me/api/portraits/men/7.jpg";
+const status2 = false;
+
+
+function App() {
+ return (
+   <div className="App">
+     <header className="App-header">
+     <Contact name={name} avatar={avatar} status={status}/>
+     <Contact name={name1} avatar={avatar1} status={status1}/>
+     <Contact name={name2} avatar={avatar2} status={status2}/>
+     </header>
+   </div>
+ );
+}
+export default App;
+
